f0cbe1e6f0cc0b50dbae55049bde51be20fccfe4
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-02-28 16:40:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-//your code here 
- function GenerateRandom(){ 
-         let x=Math.floor((Math.random() * 20) + 1); 
-         document.getElementById("num").innerHTML=x; 
-         let generatedValue=document.getElementById("num").innerHTML; 
-         let guessedValue=document.getElementById("guess").value; 
-         if(Math.abs(generatedValue-guessedValue)==0) 
-         { 
-                 document.getElementById("respond").innerHTML="Hot"; 
-         } 
-         else{ 
-                 document.getElementById("respond").innerHTML="Cold"; 
-         } 
- }
-
+var randomNum;
+var input;
+function randomNumGenerator(){
+	randomNum = Math.floor(Math.random()*41) - 20;
+	document.getElementById("num").innerHTML = randomNum;
+	guessName();
+	print();
+}
+function guessName() {
+	input = parseInt(document.getElementById("guess").value);	
+}
+function print(){
+	if(Math.abs(randomNum - input) <= 5){
+		document.getElementById("respond").innerHTML = "Hot";
+	}
+	else{
+		document.getElementById("respond").innerHTML = "Cold";
+	}
+}
